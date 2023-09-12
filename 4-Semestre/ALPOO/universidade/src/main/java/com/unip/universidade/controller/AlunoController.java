c210a5eaac7f05f77318e33566828750e24aaa65
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-12 12:36:06 -0300

    ALPOO Atividade 4

--- 4-Semestre/ALPOO/universidade/src/main/java/com/unip/universidade/controller/AlunoController.java
+++ 4-Semestre/ALPOO/universidade/src/main/java/com/unip/universidade/controller/AlunoController.java
@@ -9,54 +9,66 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.unip.universidade.model.Aluno;
-import com.unip.universidade.service.AlunoService;
+import com.unip.universidade.model.db.AlunoDAO;
 
 @Controller
 public class AlunoController {
-    private AlunoService alunoService;
+    // private AlunoService alunoService;
+    private AlunoDAO alunoDAO;
 
     @Autowired
-    public AlunoController(AlunoService alunoService) {
-        this.alunoService = alunoService;
+    // public AlunoController(AlunoService alunoService) {
+    public AlunoController(AlunoDAO alunoDAO) {
+        this.alunoDAO = alunoDAO;
     }
 
     @RequestMapping("/listaAlunos")
     public ModelAndView listarAlunos(@ModelAttribute("aluno") Aluno aluno) {
         ModelAndView modelAndView = new ModelAndView("alunos");
+        // modelAndView.addObject("listaDeAlunos", alunoService.listarAlunos());
         modelAndView.addObject("listaDeAlunos",
-                alunoService.listarAlunos());
+                alunoDAO.listarAlunos());
+        modelAndView.addObject("aluno", new Aluno());
         return modelAndView;
     }
 
     @RequestMapping("/selecionaAluno")
     public ModelAndView selecionarAlunos(@RequestParam("id") int matricula) {
-        Aluno aluno = alunoService.findAluno(matricula);
+        System.out.println("buscando o aluno: " + matricula);
+        // Aluno aluno = alunoService.findAluno(matricula);
+        Aluno aluno = alunoDAO.pesquisarAluno(matricula);
         ModelAndView modelAndView = new ModelAndView("alunos");
+        // modelAndView.addObject("listaDeAlunos", alunoService.listarAlunos());
         modelAndView.addObject("listaDeAlunos",
-                alunoService.listarAlunos());
+                alunoDAO.listarAlunos());
         modelAndView.addObject("aluno", aluno);
         return modelAndView;
     }
 
     @PostMapping("/aluno")
     public String salvarAluno(@ModelAttribute("aluno") Aluno aluno) {
-        Aluno alunoTemp = alunoService.findAluno(aluno.getMatricula());
+        // Aluno alunoTemp = alunoService.findAluno(aluno.getMatricula());
+        Aluno alunoTemp = alunoDAO.pesquisarAluno(aluno.getMatricula());
         if (alunoTemp != null) {
-            int idAlunoTemp = alunoService.listarAlunos()
-                    .indexOf(alunoTemp);
-            alunoService.listarAlunos().set(idAlunoTemp, aluno);
+            // int idAlunoTemp = alunoService.listarAlunos().indexOf(alunoTemp);
+            // alunoService.listarAlunos().set(idAlunoTemp, aluno);
+            alunoDAO.alterarAluno(aluno);
         } else
-            alunoService.listarAlunos().add(aluno);
+            // alunoService.listarAlunos().add(aluno);
+            alunoDAO.incluirAluno(aluno);
         return "redirect:/listaAlunos";
     }
 
     @RequestMapping("/removeAluno")
     public String removerAluno(@RequestParam("id") int matricula) {
-        Aluno alunoTemp = alunoService.findAluno(matricula);
-        if (alunoTemp != null) {
-            int idAlunoTemp = alunoService.listarAlunos().indexOf(alunoTemp);
-            alunoService.listarAlunos().remove(idAlunoTemp);
-        }
+        /*
+         * Aluno alunoTemp = alunoService.findAluno(matricula);
+         * if (alunoTemp != null) {
+         * int idAlunoTemp = alunoService.listarAlunos().indexOf(alunoTemp);
+         * alunoService.listarAlunos().remove(idAlunoTemp);
+         * }
+         */
+        alunoDAO.excluirAluno(matricula);
         return "redirect:/listaAlunos";
     }
 }
